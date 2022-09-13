ddf1d260280aaa33816f23c793b7d7d844df4c71
Mark Blessed <muriukimrk@gmail.com>
2022-09-13 01:51:06 -0700

    My task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(S)\n", sizeof(char));
-	printf("Size of an int: %i byte(S)\n", sizeof(int));
-	printf("Size of a long int: %i byte(S)\n", sizeof(long int));
-	printf("Size of a long long int: %i byte(S)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(S)\n", sizeof(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 
 	return (0);
 }
