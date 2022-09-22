4701ae88cb5ff7eb10be5e307b3a94f0cc9fb869
markblessed <111144903+markblessed@users.noreply.github.com>
2022-09-22 06:06:30 +0300

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int i = 0;
 
-	while (src[index])
+	for (; src[i] != '\0'; i++)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
 	}
-
 	return (dest);
 }
